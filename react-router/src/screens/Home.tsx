73a5462b5747e8ba32c994f0597f3cb787cc9655
MARUNGs <mi1764@naver.com>
2024-10-22 14:58:12 +0900

    useSearchParams

--- react-router/src/screens/Home.tsx
+++ react-router/src/screens/Home.tsx
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+
+  // URLSearchParams에 대해서
+  setTimeout(() => {
+    // search parameters를 세팅해줄 수 있다.
+    setSearchParams({
+      day: "today",
+      tomorrow: "123",
+    });
+  }, 3000);
+
+  // console.log(readSearchParams.has("day"));
+  // console.log(readSearchParams.get("day"));
+
   return (
     <div>
       <h3>Users</h3>
